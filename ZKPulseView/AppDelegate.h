47cf50b08c55fe6e97c294384424c310e094c5ed
zack <zackhsuan@gmail.com>
2014-03-16 22:52:33 +1100

    Initial Commit

---
+++ ZKPulseView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZKPulseView
+//
+//  Created by zackhsuan on 16/03/2014.
+//  Copyright (c) 2014 zzk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
